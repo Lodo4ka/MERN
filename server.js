64b4ed00bd29932c6c030bd46f6c71ff8982fa75
Lodo4ka <doomymax@gmail.com>
2018-10-01 00:56:56 +0300

    add new changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users.js')
 const profile = require('./routes/api/profile')
@@ -7,17 +8,23 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Add body-parser
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(bodyParser.json())
+
 // DB Config
 const db = require("./config/keys.js").mongoURI;
 
 // Connect to MongoDB
 mongoose
-.connect(db)
-.then(() => console.log("MongoDb Connected"))
-.catch(err => console.log(err));
+    .connect(db)
+    .then(() => console.log("MongoDb Connected"))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send("Hello");
+    res.send("Hello");
 });
 
 // USe Routes
